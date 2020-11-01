5c8eec974a15d8ea0dde7e05066f6572b853a098
FMCore <allenlydiard@gmail.com>
2020-11-01 12:59:51 -0400

    if this works, it's gonna be a miracle

--- Controllers/ShowdownController.cs
+++ Controllers/ShowdownController.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AutoModPlugins;
 using PKHeX.Core;
 using CoreAPI.Helpers;
 using PKHeX.Core.AutoMod;
